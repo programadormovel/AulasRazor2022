b1361936794707ceb4db7a3c64722cff87320fa7
programadormovel <programadormovel@gmail.com>
2023-08-30 13:05:07 -0300

    Add project files.

---
+++ AulasRazorTrainee/Pages/Formulario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AulasRazorTrainee.Pages
+{
+    public class FormularioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
